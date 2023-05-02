b1836946fe082f64045040f17dfc5bceda9d9736
boris.opachich <boris opachich>
2023-05-01 19:14:47 -0700

    201oc4.sh

---
+++ 201oc4.sh
@@ -0,0 +1,9 @@
+mkdir dir1
+mkdir dir2
+mkdir dir3
+mkdir dir4
+folders=("dir1" "dir2" "dir3" "dir4")
+touch ${folders[0]}/text.txt
+touch ${folders[1]}/text.txt
+touch ${folders[2]}/text.txt
+touch ${folders[3]}/text.txt
